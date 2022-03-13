3936dfb1f871591a1ee2e596b51ede644d19688a
Sam Billingham <sambillingham@Sams-MacBook-Air.local>
2022-03-13 20:46:53 +0100

    update latest testnet contract details

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const MERCH_DROP_CONTRACT = '0xc2a34A6B016901F07A166a6ae79dd5E8B15beCf7';
+export const MERCH_DROP_CONTRACT = '0x4b3449AaCF5C715E3E3357248c2773c544EDFd8E';
 export const INFURA_PROJECT_ID = 'b79b2af3b60447c8b444158b3ebe21eb';
 export const NETWORK_NAME = 'rinkeby';
 export const CHAIN_ID = 4;
