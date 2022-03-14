770d29e955b316be7d2c514733378e846e8f5192
Sam Billingham <sambillingham@Sams-MacBook-Air.local>
2022-03-14 14:28:23 +0100

    update contract for mainnet

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-export const MERCH_DROP_CONTRACT = '0x4b3449AaCF5C715E3E3357248c2773c544EDFd8E';
+export const MERCH_DROP_CONTRACT = '0x66722f13F6e5dcEB94c5F2aB8e6A2028039e8393';
 export const INFURA_PROJECT_ID = 'b79b2af3b60447c8b444158b3ebe21eb';
-export const NETWORK_NAME = 'rinkeby';
-export const CHAIN_ID = 4;
+export const NETWORK_NAME = 'mainnet';
+export const CHAIN_ID = 1;
 export const TOKEN_ID_MERCH_BUNDLE = 0;
